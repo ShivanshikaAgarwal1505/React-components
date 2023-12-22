603ae1d8020b51c62be5f517dd3d7c9f45978e1f
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-22 20:01:49 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import Card from './Components/card';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Card/>
+  </React.StrictMode>
+);
