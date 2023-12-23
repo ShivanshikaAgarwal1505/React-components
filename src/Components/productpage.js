7f0dbed7e9ace47ccd27b3f80ed1df5fb22bb10f
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-23 21:19:44 +0530

    Add files via upload

---
+++ src/Components/productpage.js
@@ -0,0 +1,18 @@
+import "./productpage.css";
+
+// design idea- https://in.pinterest.com/pin/618189486326098483/
+const Product=()=>{
+<div id="page">
+    <div id="main">
+        {/* <h1>original t-shirt </h1>
+        <div class="sub-text">somethings are definetly worthy to buy</div>
+        <div class="price">Rs. 300</div> */}
+    </div>
+    <div id="side">
+        <img src="../alex-haigh-fEt6Wd4t4j0-unsplash-removebg-preview.png" class="img" />
+        <div class="trapezoid"></div>
+    </div>
+</div>
+}
+
+export default Product;
