9702e2e053c0e1d470ada63154376eac883aee26
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-24 00:08:37 +0530

    Add files via upload

--- src/Components/productpage.js
+++ src/Components/productpage.js
@@ -1,18 +1,34 @@
 import "./productpage.css";
-
+import image from "../alex-haigh-fEt6Wd4t4j0-unsplash-removebg-preview.png";
 // design idea- https://in.pinterest.com/pin/618189486326098483/
+
 const Product=()=>{
-<div id="page">
-    <div id="main">
-        {/* <h1>original t-shirt </h1>
-        <div class="sub-text">somethings are definetly worthy to buy</div>
-        <div class="price">Rs. 300</div> */}
-    </div>
-    <div id="side">
-        <img src="../alex-haigh-fEt6Wd4t4j0-unsplash-removebg-preview.png" class="img" />
-        <div class="trapezoid"></div>
-    </div>
-</div>
+    return (
+      <div id="page" class="product">
+        <div id="main">
+          <h2>original t-shirt </h2>
+          <div class="product-sub-text">
+            somethings are definetly worthy to buy
+          </div>
+          <div class="product-price">Rs. 300</div>
+          <div class="product-body">
+            <h6>benifits</h6>
+            <ul class="product-list">
+              <li>DRI-fit mesh fabric.</li>
+              <li>Details on front.</li>
+              <li>Rib crew neck.</li>
+            </ul>
+          </div>
+          <div class="product-footer">
+            
+          </div>
+        </div>
+        <div id="side">
+          <img src={image} class="img1" />
+          <div class="trapezoid"></div>
+        </div>
+      </div>
+    );
 }
 
 export default Product;
